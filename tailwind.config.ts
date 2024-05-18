6261d1d662a8c6644c946110fab985d7ad8232e2
peltastic <pelumi.o@whispahealth.com>
2024-05-18 09:22:17 +0100

    base template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "F5F6FA",
+      black1: "#131316",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      des: "1200px",
+      xl: "1440px",
+      xxl: "1600px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

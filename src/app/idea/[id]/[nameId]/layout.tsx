77da8bdc8df219e13ac0696caa06505aa84420eb
peltastic <onosogapelumi@gmail.com>
2024-09-11 03:59:06 +0100

    updates

--- src/app/idea/[id]/[nameId]/layout.tsx
+++ src/app/idea/[id]/[nameId]/layout.tsx
@@ -9,30 +9,23 @@ type Props = {
   };
 };
 
-export const metadata: Metadata = {
-  openGraph: {
-    title: "sjsj",
-    images:
-      "https://ideas-for-africa-deploy.vercel.app/api/og?image=https://ideaafricabucket.s3.eu-north-1.amazonaws.com/1724208567753-75589356-AAAABeLOP2XHAUHr9G2NUNFSiOjQiontLUh2PMu2bdFu3Qf-SA5G_CKYDMGv_c1kavKLhqlS6xSrA0SCM5zOvQ8YUtABF8DzU1C8WrMd.jpg",
-  },
-};
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const idea = await axios.get(
-//     `https://api.ideasafrica.org/api/users/ideas/${params.id}`
-//   );
-//   if (!idea) {
-//     return {
-//       title: "Idea post not found",
-//     };
-//   }
-//   return {
-//     openGraph: {
-//       title: idea.data.idea.headline,
-//       images: `https://ideas-for-africa-deploy.vercel.app/api/og?image=${idea.data.thumbs[0].path}`,
-//     },
-//   };
-// }
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const idea = await axios.get(
+    `https://api.ideasafrica.org/api/users/ideas/${params.id}`
+  );
+  if (!idea) {
+    return {
+      title: "Idea post not found",
+    };
+  }
+  return {
+    openGraph: {
+      title: idea.data.idea.headline,
+    //   images: `https://ideas-for-africa-deploy.vercel.app/api/og?image=${idea.data.thumbs[0].path}`,
+    },
+  };
+}
 
 //https://api.ideasafrica.org/api/users/ideas/b02aa963-9c15-49c5-b9d6-a890422df3aa
 
